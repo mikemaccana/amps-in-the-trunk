31b12f3689fc96ab2e88a31858d19feae2ef5dba
Mike MacCana <mike@certsimple.com>
2016-08-18 14:52:57 +0100

    Add tests

---
+++ test/tests.js
@@ -0,0 +1,38 @@
+// Tests. Mocha TDD/assert style. See
+// http://visionmedia.github.com/mocha/
+// http://nodejs.org/docs/latest/api/assert.html
+
+var parentDir = __dirname,
+	assert = require('assert'),
+	fs = require('fs'),
+	ampsInTheTrunk = require('../index.js')(),
+	dedent = require('dedent'),
+	log = console.log.bind(console)
+
+suite('amp-img tags', function(){
+
+	test('makes img tags into amp-img tags', function(){
+		var testImage = parentDir+'/ferret.gif';
+		var stats = fs.statSync(testImage);
+		var html = dedent(`
+			<div>
+				<p>
+					I've gone into full girl meltdown.
+				</p>
+
+				<img src="`+testImage+`"/>
+			</div>
+		`)
+		var actual = ampsInTheTrunk.toAmp(html);
+		var expected = dedent(`
+		<div>
+				<p>
+					I&apos;ve gone into full girl meltdown.
+				</p>
+
+				<amp-img layout="responsive" alt="an image" src="/Users/mike/Documents/amps-in-the-trunk/test/ferret.gif"></amp-img>
+			</div>`)
+		assert.deepEqual(actual, expected)
+	});
+
+})
